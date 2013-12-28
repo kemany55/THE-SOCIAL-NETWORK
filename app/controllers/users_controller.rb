59905fdc98e3a784f76c5888a73bbd618fef8a59
kemany55 <kemany55@gmail.com>
2013-12-28 03:20:41 +0200

    Social Network

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+  end
+
+  def new
+    @user = User.new
+  end
+ def create
+    @user = User.new(user_params)
+    if @user.save
+    sign_in @user
+flash[:success] = "Welcome to Dr. Ameen Shoukry's untitled project!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'f2ee16148501e33bc8281980885055bc324d433c1827ff11ff515b2cce9f66083fbefbe3842b973831195017a202b0cbaa660d0d631e9bed7166485c554423db'
